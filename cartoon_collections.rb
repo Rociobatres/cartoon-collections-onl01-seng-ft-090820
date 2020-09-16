81f69d81c8910319c1799d965be60e453977dfe0
Rocio Batres <jonasgirl21@gmail.com>
2020-09-16 01:18:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,10 +20,10 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(foods)
-  foods.include?("cheddar") 
-  if true 
-    puts "cheddar"
-  else
-    nil 
-  end   
+  foods.each do |food| foods.include?("cheddar") 
+    if true 
+      puts "cheddar"
+    else
+      nil 
+    end   
 end
